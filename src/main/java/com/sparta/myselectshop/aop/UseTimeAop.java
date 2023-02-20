c1eda88fb028d6989527b62de9a18828336e76e1
DOGWANNA <9401ndk@naver.com>
2023-02-20 17:14:31 +0900

    feat: 항해99 주특기 숙련주차 실습 AOP, 예외처리 추가

---
+++ src/main/java/com/sparta/myselectshop/aop/UseTimeAop.java
@@ -0,0 +1,66 @@
+package com.sparta.myselectshop.aop;
+
+import com.sparta.myselectshop.entity.ApiUseTime;
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.repository.ApiUseTimeRepository;
+import com.sparta.myselectshop.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.Synchronized;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class UseTimeAop {
+
+    private final ApiUseTimeRepository apiUseTimeRepository;
+
+    @Around("execution(public * com.sparta.myselectshop.controller..*(..))")
+    //mySelectSHop의 모든 컨트롤러 부분에 다 동작을 시키겠다는 내용
+    public synchronized Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        // 측정 시작 시간
+        long startTime = System.currentTimeMillis();
+
+        try {
+            // 핵심기능 수행
+            Object output = joinPoint.proceed(); // 잡아오는 Controller API 로직부분이 실행된다.
+            return output;
+        } finally {
+            // 측정 종료 시간
+            long endTime = System.currentTimeMillis();
+            // 수행시간 = 종료 시간 - 시작 시간
+            long runTime = endTime - startTime;
+
+            // 로그인 회원이 없는 경우, 수행시간 기록하지 않음
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            // 로그인 정보 확인해서 로그인 여부 확인함.
+            if (auth != null && auth.getPrincipal().getClass() == UserDetailsImpl.class) {
+                // 로그인 회원 정보
+                UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
+                User loginUser = userDetails.getUser();
+
+                // API 사용시간 및 DB 에 기록
+                ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser)
+                        .orElse(null);
+                if (apiUseTime == null) {
+                    // 로그인 회원의 기록이 없으면
+                    apiUseTime = new ApiUseTime(loginUser, runTime);
+                } else {
+                    // 로그인 회원의 기록이 이미 있으면
+                    apiUseTime.addUseTime(runTime);
+                }
+
+                log.info("[API Use Time] Username: " + loginUser.getUsername() + ", Total Time: " + apiUseTime.getTotalTime() + " ms");
+                apiUseTimeRepository.save(apiUseTime);
+
+            }
+        }
+    }
+}
